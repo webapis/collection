a85f11feabdee973ce42f6e33ad71afe116b3325
webapis <webapis.github@gmail.com>
2022-01-24 16:47:04 +0500

    v

--- collection-component.js
+++ collection-component.js
@@ -4,16 +4,16 @@ customElements.define('collection-component', class extends HTMLElement{
     }
 
     async connectedCallback(){
-        this.innerHTML=`<div id="container"></div>`
+        this.innerHTML=`<div class="container-fluid"><div id="container" class="container"></div></div>`
         const response = await fetch('https://res.cloudinary.com/codergihub/raw/upload/collection2022.json',{cache:'reload'})
         const data = await response.json()
-        const collection = data.filter((d,i)=> i<5)
-        const cloudinaryUrl ='https://res.cloudinary.com/codergihub/image/fetch/h_200/'
+        const collection = data.filter((d,i)=> i<15)
+        const cloudinaryUrl ='https://res.cloudinary.com/codergihub/image/fetch/w_200/'
         collection.forEach(col=>{
             const {imageUrl,priceBasket,priceNew,priceOld,title,link}=col
-         debugger;
+     
             document.getElementById('container').insertAdjacentHTML('beforeend',`
-           <product-component imageUrl="${imageUrl}" priceBasket="${priceBasket}" priceNew="${priceNew}" priceOld="${priceOld}"
+           <product-component class="item" imageUrl="${imageUrl}" priceBasket="${priceBasket}" priceNew="${priceNew}" priceOld="${priceOld}"
            title ="${title}" link="${link}"
            ></product-component>
             `)
@@ -29,7 +29,8 @@ customElements.define('product-component', class extends HTMLElement{
     }
 
     async connectedCallback(){
-        const cloudinaryUrl ='https://res.cloudinary.com/codergihub/image/fetch/h_200/co_rgb:FFFFFF,l_text:Verdana_15_underline_letter_spacing_3:DeFacto/fl_layer_apply,g_south,y_0.05/'
+      
+        const cloudinaryUrl =`https://res.cloudinary.com/codergihub/image/fetch/w_${this.size()}/co_rgb:FFFFFF,l_text:Verdana_15_underline_letter_spacing_0:www.defacto.com.tr/fl_layer_apply,g_south,y_0.0/`
         const imageUrl =this.getAttribute('imageUrl')
         const priceBasket =this.getAttribute('priceBasket')
         const priceNew =this.getAttribute('priceNew')
@@ -37,17 +38,25 @@ customElements.define('product-component', class extends HTMLElement{
         const title =this.getAttribute('title')
         const link =this.getAttribute('link')
 
-        this.innerHTML=`<div>
-        <a href="${link}">
-        <img src="${cloudinaryUrl+imageUrl}"/>
-        <div>${title}</div>
-     
-        </a>
-        <div>priceBasket:${priceBasket}</div>
-        <div>priceNew:${priceNew}</div>
-        <div>priceOld:${priceOld}</div>
+        this.innerHTML=`
+    
+  
       
-        </div>`
+  <img src="${cloudinaryUrl+imageUrl}" class="card-img-top" alt="d">
+
+
+        `
 
     }
+
+    size(){
+        if(window.innerWidth<=375){
+          return '150'
+        } else{
+
+            return '250'
+        }
+    }
+
+
 })
