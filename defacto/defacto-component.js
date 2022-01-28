cd63c905e9d7f19ef953752ec9ab7d8c67cbf8f2
webapis <webapis.github@gmail.com>
2022-01-28 16:46:44 +0500

    v

--- defacto/defacto-component.js
+++ defacto/defacto-component.js
@@ -9,6 +9,8 @@ customElements.define('defacto-component', class extends HTMLElement{
         <a class="" href="#" id="defacto-kadin-yeni-gelenler">Yeni gelenler</a>
         <a class="" href="#" id="koton-kadin-elbise">Koton Kadin Elbise</a>
         <a class="" href="#" id="defacto-kadin-elbise">Defacto Kadin Elbise</a>
+        <a class="" href="#" id="boyner-kadin-elbise">Boyner Kadin Elbise</a>
+        <a class="" href="#" id="lcwaikiki-kadin-elbise">LCWAIKIKI Kadin Elbise</a>
         </div>`
        this.querySelectorAll('a').forEach(element=>{
            element.addEventListener('click',async e=>{
