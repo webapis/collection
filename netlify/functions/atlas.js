ee65e6ac72f578571b341cc39ef47abfa81fec1d
webapis <webapis.github@gmail.com>
2022-01-31 12:07:34 +0500

    dd

--- netlify/functions/atlas.js
+++ netlify/functions/atlas.js
@@ -2,13 +2,15 @@ const { MongoClient } = require('mongodb');
 
 const uri = "mongodb+srv://webapis:Dragonfly1977@cluster0.8svj0.mongodb.net/ecom?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-exports.handler=async function (event,context){
- const clnt= await   client.connect()
- const collection =clnt.db("ecom").collection("collection2023");
- const response = await collection.find().toArray()
-return {
-    statusCode:200,
-    body:JSON.stringify({data:response})
-}
+
+exports.handler = async function (event, context) {
+    const clnt = await client.connect()
+    const collection = clnt.db("ecom").collection("collection2023");
+    const response = await collection.findOne()
+    clnt.close()
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ data: response })
+    }
 }
 
