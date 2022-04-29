e34d0a96ddacaccf236869b0bf07209723b92da5
webapis <60083568+webapis@users.noreply.github.com>
2022-04-29 11:33:19 +0500

    v

--- netlify/functions/atlas.js
+++ netlify/functions/atlas.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const {nodeFetch}=require('../../nodefetch')
+const { nodeFetch } = require('../../nodefetch')
 
 
 const headers = {
@@ -12,36 +12,31 @@ const headers = {
 exports.handler = async function (event, context) {
     const { gender, subcategory, page, category } = event.queryStringParameters
     let spreadsheetId = ''
+    const start =page ==='100'? '2': parseInt(page)-99
 debugger;
-if(gender){
-    switch (gender) {
-        case 'erkek':
-            spreadsheetId = '1IeaYAURMnrbZAsQA_NO_LA_y_qq8MmwxjSo854vz5YM'
-            break;
-        case 'kadın':
-            spreadsheetId = '12mKtqxu5A-CVoXP_Kw36JxKiC69oPUUXVQmm7LUfh3s'
-            break;
-        default:
+    if (gender) {
+        switch (gender) {
+            case 'erkek':
+                spreadsheetId = '1IeaYAURMnrbZAsQA_NO_LA_y_qq8MmwxjSo854vz5YM'
+                break;
+            case 'kadın':
+                spreadsheetId = '12mKtqxu5A-CVoXP_Kw36JxKiC69oPUUXVQmm7LUfh3s'
+                break;
+            default:
+        }
 
-
-    }
-
+        const response = await nodeFetch({ host: 'sheets.googleapis.com', path: `/v4/spreadsheets/${spreadsheetId}/values/DATA!A${start}:P${page}?majorDimension=ROWS&key=AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA`, method: 'GET', headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json' } })
 debugger;
- const response =await nodeFetch({host:'sheets.googleapis.com',path:`/v4/spreadsheets/${spreadsheetId}/values/DATA!A2%3AP50?majorDimension=ROWS&key=AIzaSyDb8Z27Ut0WJ-RH7Exi454Bpit9lbARJeA`, method:'GET',headers: { 'User-Agent': 'node.js', 'Content-Type': 'application/json' }})
-
-debugger;
- 
-    debugger;
-    return {
-        statusCode: 200, headers,
-        body: response
+        return {
+            statusCode: 200, headers,
+            body: response
+        }
+    } else {
+        return {
+            statusCode: 401, headers,
+            body: ''
+        }
     }
-} else{
-    return {
-        statusCode: 401, headers,
-        body: ''
-    }
-}
 
 
 }
